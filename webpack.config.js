344222b43765f7f2f71606a97c2fbbc5d67222b9
Germain Loret <germain.loret@gmail.com>
2019-09-19 10:04:55 +0200

    Load scss through webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/javascripts/application.js",
   mode: "development",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+    ],
   },
   devtool: "sourcemap"
 };
